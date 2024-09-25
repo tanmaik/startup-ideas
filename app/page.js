bfa4c5ef3689f6dbf44cb475f8aafa5d2356835d
Sumanth Kalluru <kallurusumanth@gmail.com>
2024-09-25 04:41:30 -0500

    authorization

--- app/page.js
+++ app/page.js
@@ -1,10 +1,19 @@
-import NotionList from './client';
+import NotionList from './components/client';
 
 export default function Home() {
+
+  const isAuthorized = true;
+
   return (
     <div className="p-6 max-w-3xl mx-auto">
       <h1 className="text-3xl font-bold mb-6">Ideas</h1>
-      <NotionList />
+      {isAuthorized ? (
+        <NotionList />
+      ) : (
+        <div>
+          <p>You are not authorized to access this page.</p>
+        </div>
+      )}
     </div>
   );
 }
