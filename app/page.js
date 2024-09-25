8aa0a25031f0338e9cc46ca059ae7f5d10e38529
Sumanth Kalluru <kallurusumanth@gmail.com>
2024-09-25 04:30:34 -0500

    List

--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
+import NotionList from './client';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="p-6 max-w-3xl mx-auto">
+      <h1 className="text-3xl font-bold mb-6">Ideas</h1>
+      <NotionList />
+    </div>
+  );
 }
